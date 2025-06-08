54bf9bad35365468ae39dc6ed98e1c155ce9ca7b
Abhishek Athare <athareal2001@gmail.com>
2025-06-08 18:26:45 +0530

    feat: Add course search functionality with navigation and course list

--- frontend/src/pages/Student/Home.jsx
+++ frontend/src/pages/Student/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Hero from '../../components/Student/Hero'
+import Companies from '../../components/Student/Companies'
+import CoursesSection from '../../components/Student/CoursesSection'
 
 function Home() {
   return (
-    <div>Home Page - Student</div>
+    <div className='flex flex-col items-center space-y-7 text-center'>
+      <Hero />
+      <Companies/>
+      <CoursesSection/>
+    </div>
   )
 }
 
